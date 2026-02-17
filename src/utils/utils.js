89a3b5a2c9737fd22aa1a8a0a4581e91d9e1994b
Gabriel Gomez <gomez.50057@gmail.com>
2026-02-17 12:03:20 -0600

    orden de los ganaderes en alfabeto

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,34 +1,14 @@
 export const datosBibliotecaDigital = {
   cards: [
     {
-      "name": "Metodología Automatizada para el Seguimiento y Evaluación del PMD mediante una Matriz Multicriterio con Pesos Ponderados",
-      "municipio": ["San Felipe Orizatlán"],
-      "description": "El proyecto consiste en una matriz la cual permite calcular puntajes, priorizar actividades y generar un porcentaje real de avance por línea de acción, estrategia y objetivo del Plan Municipal de Desarrollo.",
+      "name": '"Generando la Identidad de tu Calle" - Nomenclatura de calles',
+      "municipio": ["El Arenal"],
+      "description": " El proyecto consiste en generar mecanismos de participación ciudadana para nombrar oficialmente las calles, avenidas y vialidades fomentando la identidad cultural y orden urbano.",
       "año": "2025",
-      "ftSrc": "pdfs/edicion2025/fichaTecnica/Ficha Técnica San Felipe Orizatlán.pdf",
-      "peSrc": "pdfs/edicion2025/presentacion/Presentación San Felipe Orizatlán.pdf",
-      "imageName": "edicion2025/San Felipe Orizatlán 2025.jpg",
-      "projectsIndex": "6"
-    },
-    {
-      "name": "Planeación Democrática: La Ciencia de Datos aplicada para la toma de decisiones",
-      "municipio": ["Pachuca de Soto"],
-      "description": "Proceso que incorpora el modelo de ciencia de datos para jerarquizar, y analizar los problemas más urgentes en el municipio, así como la incorporación de las Tecnologías de Información y Comunicaciones en la plataforma de participación ciudadana.",
-      "año": "2025",
-      "ftSrc": "pdfs/edicion2025/fichaTecnica/Ficha Técnica Pachuca.pdf",
-      "peSrc": "pdfs/edicion2025/presentacion/Presentación Pachuca de Soto.pdf",
-      "imageName": "edicion2025/Pachuca de Soto 2025.jpg",
-      "projectsIndex": "7"
-    },
-    {
-      "name": "Implementación y seguimiento del Presupuesto Basado en Resultados (PBR), en conjunto con el Observatorio Ciudadano de Hidalgo A.C. a través del mecanismo Comité de Evaluación Municipal",
-      "municipio": ["Omitlán de Juárez"],
-      "description": "El proyecto consiste en que las instancias municipales gubernamentales y sociedad civil trabajen conjuntamente a través de la participación ciudadana, en el proceso de diseño, implementación, así como el seguimiento, evaluación de programas y proyectos presupuestarios, con el objetivo de mejorar la transparencia y rendición de cuentas.",
-      "año": "2025",
-      "ftSrc": "pdfs/edicion2025/fichaTecnica/Ficha Técnica Omitlán de Juárez.pdf",
-      "peSrc": "pdfs/edicion2025/presentacion/Presentación Omitlán de Juárez.pdf",
-      "imageName": "edicion2025/Omitlán de Juárez 2025.jpg",
-      "projectsIndex": "8"
+      "ftSrc": "pdfs/edicion2025/fichaTecnica/Ficha Técnica El Arenal.pdf",
+      "peSrc": "pdfs/edicion2025/presentacion/Presentación El Arenal.pdf",
+      "imageName": "edicion2025/El Arenal 2025.jpg",
+      "projectsIndex": "10"
     },
     {
       "name": "Programa Municipal de Población 2024-2027",
@@ -41,14 +21,44 @@ export const datosBibliotecaDigital = {
       "projectsIndex": "9"
     },
     {
-      "name": '"Generando la Identidad de tu Calle" - Nomenclatura de calles',
-      "municipio": ["El Arenal"],
-      "description": " El proyecto consiste en generar mecanismos de participación ciudadana para nombrar oficialmente las calles, avenidas y vialidades fomentando la identidad cultural y orden urbano.",
+      "name": "Implementación y seguimiento del Presupuesto Basado en Resultados (PBR), en conjunto con el Observatorio Ciudadano de Hidalgo A.C. a través del mecanismo Comité de Evaluación Municipal",
+      "municipio": ["Omitlán de Juárez"],
+      "description": "El proyecto consiste en que las instancias municipales gubernamentales y sociedad civil trabajen conjuntamente a través de la participación ciudadana, en el proceso de diseño, implementación, así como el seguimiento, evaluación de programas y proyectos presupuestarios, con el objetivo de mejorar la transparencia y rendición de cuentas.",
       "año": "2025",
-      "ftSrc": "pdfs/edicion2025/fichaTecnica/Ficha Técnica El Arenal.pdf",
-      "peSrc": "pdfs/edicion2025/presentacion/Presentación El Arenal.pdf",
-      "imageName": "edicion2025/El Arenal 2025.jpg",
-      "projectsIndex": "10"
+      "ftSrc": "pdfs/edicion2025/fichaTecnica/Ficha Técnica Omitlán de Juárez.pdf",
+      "peSrc": "pdfs/edicion2025/presentacion/Presentación Omitlán de Juárez.pdf",
+      "imageName": "edicion2025/Omitlán de Juárez 2025.jpg",
+      "projectsIndex": "8"
+    },
+    {
+      "name": "Planeación Democrática: La Ciencia de Datos aplicada para la toma de decisiones",
+      "municipio": ["Pachuca de Soto"],
+      "description": "Proceso que incorpora el modelo de ciencia de datos para jerarquizar, y analizar los problemas más urgentes en el municipio, así como la incorporación de las Tecnologías de Información y Comunicaciones en la plataforma de participación ciudadana.",
+      "año": "2025",
+      "ftSrc": "pdfs/edicion2025/fichaTecnica/Ficha Técnica Pachuca.pdf",
+      "peSrc": "pdfs/edicion2025/presentacion/Presentación Pachuca de Soto.pdf",
+      "imageName": "edicion2025/Pachuca de Soto 2025.jpg",
+      "projectsIndex": "7"
+    },
+    {
+      "name": "Metodología Automatizada para el Seguimiento y Evaluación del PMD mediante una Matriz Multicriterio con Pesos Ponderados",
+      "municipio": ["San Felipe Orizatlán"],
+      "description": "El proyecto consiste en una matriz la cual permite calcular puntajes, priorizar actividades y generar un porcentaje real de avance por línea de acción, estrategia y objetivo del Plan Municipal de Desarrollo.",
+      "año": "2025",
+      "ftSrc": "pdfs/edicion2025/fichaTecnica/Ficha Técnica San Felipe Orizatlán.pdf",
+      "peSrc": "pdfs/edicion2025/presentacion/Presentación San Felipe Orizatlán.pdf",
+      "imageName": "edicion2025/San Felipe Orizatlán 2025.jpg",
+      "projectsIndex": "6"
+    },
+    {
+      "name": "Observatorio de Políticas Públicas",
+      "municipio": ["alfajayucan"],
+      "description": "El proyecto consiste en un módulo de Observatorio de Políticas Públicas que brinda información estratégica sobre el avance en la atención de problemas públicos con temáticas alineadas al Plan Municipal de Desarrollo y con dos componentes Diagnóstico y Acciones y Resultados, con el objetivo de mejorar la transparencia y rendición de cuentas del ejercicio del gasto público.",
+      "año": "2023",
+      "ftSrc": "https://drive.google.com/file/d/1-ZjpyZUnZfVSVAD7Ayi1ut2eEv0wiwJn/view?usp=sharing",
+      "peSrc": "https://drive.google.com/file/d/1acVD8pHTXgPp2MBBs2L06rktDcKllYv-/view?usp=sharing",
+      "imageName": "observatorioPoliticasPublicas_006.jpg",
+      "projectsIndex": "5"
     },
     {
       "name": "Comités Vecinales - Visión Plural para el Desarrollo Municipal",
@@ -58,7 +68,7 @@ export const datosBibliotecaDigital = {
       "ftSrc": "https://drive.google.com/file/d/16MMmKWs44mL3Vu2k8oherIeTOwJgML1j/view?usp=sharing",
       "peSrc": "https://drive.google.com/file/d/1diMwlq4ji-hc-jL27ffq_NCy7w2xRE60/view?usp=sharing",
       "imageName": "ComitesVecinales_051.jpg",
-      "projectsIndex": "1"
+      "projectsIndex": "4"
     },
     {
       "name": "Plataformas de Participación Ciudadana para la Construcción y Actualización del PMD 2020-2023",
@@ -68,6 +78,16 @@ export const datosBibliotecaDigital = {
       "ftSrc": "https://drive.google.com/file/d/1n76uMhyQSX5ZSkN43UT4gG5-7iICuWLV/view?usp=sharing",
       "peSrc": "https://drive.google.com/file/d/1f8j33GabW9HPgdVKJIz3blY7Qs_MvIgv/view?usp=drive_link",
       "imageName": "plataformasParticipacion_048.jpg",
+      "projectsIndex": "3"
+    },
+    {
+      "name": "Sistema de Evaluación del Desempeño",
+      "municipio": ["tizayuca"],
+      "description": "El Sistema de Evaluación de Desempeño del municipio es un instrumento utilizado para conocer el grado de cumplimiento de los resultados de los programas presupuestarios ejecutados por las unidades administrativas de la administración pública municipal, y tiene como finalidad lograr el cumplimiento de objetivos institucionales a través del seguimiento, monitoreo y evaluación de los indicadores estratégicos y de gestión, de los programas, planes y políticas públicas ejecutadas por la administración pública municipal.",
+      "año": "2023",
+      "ftSrc": "https://drive.google.com/file/d/1lVxO0L5KcfKuDaoUVyb3GBbeSue5o881/view?usp=sharing",
+      "peSrc": "https://drive.google.com/file/d/1dtU2ahN8nxI2d_B3pwfrBup_Uq2zoX6F/view?usp=sharing",
+      "imageName": "sistemaEvaluacion_069.jpg",
       "projectsIndex": "2"
     },
     {
@@ -78,27 +98,7 @@ export const datosBibliotecaDigital = {
       "ftSrc": "https://drive.google.com/file/d/1BazpOVCmj5CYxB8UeJRqxtlQjYWJqurD/view?usp=sharing",
       "peSrc": "https://drive.google.com/file/d/17vRAM85J5Zb7cAztneEhW6tqPTF34cUs/view?usp=sharing",
       "imageName": "matrizIndicadores_066.jpg",
-      "projectsIndex": "3"
-    },
-    {
-      "name": "Observatorio de Políticas Públicas",
-      "municipio": ["alfajayucan"],
-      "description": "El proyecto consiste en un módulo de Observatorio de Políticas Públicas que brinda información estratégica sobre el avance en la atención de problemas públicos con temáticas alineadas al Plan Municipal de Desarrollo y con dos componentes Diagnóstico y Acciones y Resultados, con el objetivo de mejorar la transparencia y rendición de cuentas del ejercicio del gasto público.",
-      "año": "2023",
-      "ftSrc": "https://drive.google.com/file/d/1-ZjpyZUnZfVSVAD7Ayi1ut2eEv0wiwJn/view?usp=sharing",
-      "peSrc": "https://drive.google.com/file/d/1acVD8pHTXgPp2MBBs2L06rktDcKllYv-/view?usp=sharing",
-      "imageName": "observatorioPoliticasPublicas_006.jpg",
-      "projectsIndex": "4"
-    },
-    {
-      "name": "Sistema de Evaluación del Desempeño",
-      "municipio": ["tizayuca"],
-      "description": "El Sistema de Evaluación de Desempeño del municipio es un instrumento utilizado para conocer el grado de cumplimiento de los resultados de los programas presupuestarios ejecutados por las unidades administrativas de la administración pública municipal, y tiene como finalidad lograr el cumplimiento de objetivos institucionales a través del seguimiento, monitoreo y evaluación de los indicadores estratégicos y de gestión, de los programas, planes y políticas públicas ejecutadas por la administración pública municipal.",
-      "año": "2023",
-      "ftSrc": "https://drive.google.com/file/d/1lVxO0L5KcfKuDaoUVyb3GBbeSue5o881/view?usp=sharing",
-      "peSrc": "https://drive.google.com/file/d/1dtU2ahN8nxI2d_B3pwfrBup_Uq2zoX6F/view?usp=sharing",
-      "imageName": "sistemaEvaluacion_069.jpg",
-      "projectsIndex": "5"
+      "projectsIndex": "1"
     }
   ]
 };
